d2c511ad1caa1ed1fafff5f5a7cdf9d5284eb12f
AlexandrHeroCoud <polkanov.freelancer@gmail.com>
2020-08-24 06:38:49 +0300

    small debug https://docs.google.com/document/d/1ItcwhHt5idMLLIpkIBOq-cFNUYtbLM1oRU9Mqpl5eq0/edit

---
+++ src/Components/Buyer/Buyer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Container} from "@material-ui/core";
+
+const Buyer = (props) =>{
+    return (
+        <Container>
+            <h1>asdasdasd</h1>
+        </Container>
+    )
+}
+export default Buyer
